6b2aafb71492ea581be4bd07c4c5c0155fd8de9a
AAB <aab@AABs-iMac.local>
2023-01-11 11:06:39 -0600

    Removed unneccessary comments and console.log messages for testing from designs.js

--- designs.js
+++ designs.js
@@ -1,15 +1,12 @@
 // Create objects for gridSize and canvas
 const gridSize = document.querySelector('#sizePicker');
 const canvas = document.querySelector('#pixelCanvas');
-// var color = document.querySelector('#colorPicker');
 
 //Call to create default grid when page loads
 makeGrid(20,20);
 
-// When size is submitted by the user, call makeGrid()
 // Catch submit to create canvas
 gridSize.addEventListener('submit',function(e){
-    console.log("hey its been clicked!");
     e.preventDefault();
     while(canvas.hasChildNodes()){
         canvas.removeChild(canvas.firstChild);
@@ -28,7 +25,6 @@ function makeGrid(width, height) {
             let canvasUnit = row.insertCell(y);
             canvasUnit.style.backgroundColor = "white";
             canvasUnit.addEventListener('click', function(e){
-                console.log("hey its been clicked!");
                 e.preventDefault();
                 color = document.querySelector('#colorPicker');
                 canvasUnit.style.backgroundColor = color.value;
